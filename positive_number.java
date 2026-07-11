101f1fe5453a531bee4faaac3b1f70b0604998f0
PiyakshiX <piyakshisaikia@gmail.com>
2026-07-11 18:48:08 +0530

    Add files via upload

---
+++ positive_number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class positive_number {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        if(number>0){
+            System.out.println("The number is positive.");
+        }
+        else{
+            System.out.println("The number is not positive.");
+        }
+        sc.close();
+    }
+}
