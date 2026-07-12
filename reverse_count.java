e60a499dd29968b629398d5958e5e2eccaf42721
PiyakshiX <piyakshisaikia@gmail.com>
2026-07-12 18:01:42 +0530

    Add files via upload

---
+++ reverse_count.java
@@ -0,0 +1,7 @@
+public class reverse_count {
+    public static void main(String[] args){
+        for(int i=10;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
---
+++ switchcase.java
@@ -0,0 +1,19 @@
+public class switchcase {
+    public static void main(String[] args) {
+        String fruit = "banana";
+        switch(fruit) {
+            case "apple":
+                System.out.println("This is an apple.");
+                break;
+            case "banana":
+                System.out.println("This is a banana.");
+                break;
+            case "orange":
+                System.out.println("This is an orange.");
+                break;
+            default:
+                System.out.println("Unknown fruit.");
+        }
+    
+    }
+}
