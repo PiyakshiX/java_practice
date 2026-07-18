136517a59ddc91f050bd98fe2c3f0f9e220d63a0
PiyakshiX <piyakshisaikia@gmail.com>
2026-07-18 17:32:53 +0530

    Add files via upload

---
+++ loopstring.java
@@ -0,0 +1,8 @@
+public class loopstring {
+    public static void main(String[] args){
+        String name="Java";
+        for(int i=0; i<name.length(); i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
---
+++ stringExample.java
@@ -0,0 +1,8 @@
+public class stringExample {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = str1 + " " + str2;
+        System.out.println(str3);
+    }
+}
